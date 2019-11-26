5581d87b3828cea75fd586087a2113c5dde8a473
Ansh Goyal <ansh_goyal@hotmail.com>
2019-11-26 09:46:24 +0530

    Front End Bug Fixes: * Serial Number Not in Order * Photograph Not Visible without restart

--- src/main/webapp/js/studentlist.js
+++ src/main/webapp/js/studentlist.js
@@ -6,7 +6,7 @@ $.get(api, function (student, status) {
             var domainName = student[i].domain.discipline + " " + student[i].domain.branch;
             var photograph = '<img src="' + student[i].photograph + '" height="80" alt="' + student[i].rollNumber + '">'
             student_data_body += '<tr>'
-                + '<td>' + i + 1 + '</td>'
+                + '<td>' + (i + 1) + '</td>'
                 + '<td>' + student[i].rollNumber + '</td>'
                 + '<td>' + student[i].firstName + '</td>'
                 + '<td>' + student[i].middleName + '</td>'
